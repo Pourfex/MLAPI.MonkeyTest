21432284cbffa15077e75f8ea4671be24375a93d
pourfex <alexis.delforges@gmail.com>
2020-05-28 09:30:52 +0200

    Initial commit - 1000 simple ok , 1500 complex gives error

---
+++ Assets/ComplexNetworkScript.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using MLAPI;
+using MLAPI.NetworkedVar.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ComplexNetworkScript : NetworkedBehaviour
+{
+
+    public int numberOfPoints = 115;
+
+    public float delayBetweenTwoPoints = 0.3f;
+    
+    public NetworkedList<Vector3> networkedPosition = new NetworkedList<Vector3>(new MLAPI.NetworkedVar.NetworkedVarSettings()
+    {
+        SendChannel = "Reliable",
+        ReadPermission = MLAPI.NetworkedVar.NetworkedVarPermission.Everyone,
+        WritePermission = MLAPI.NetworkedVar.NetworkedVarPermission.Everyone,
+        SendTickrate = 10
+    }, new List<Vector3>());
+
+    public List<Vector3> positions = new List<Vector3>();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (IsOwner)
+        {
+            StartCoroutine(AddValueToList(numberOfPoints, delayBetweenTwoPoints));
+        }
+    }
+
+    private IEnumerator AddValueToList(int numberOfPoints, float delayBetweenTwoPoints)
+    {
+        var awaiter = new WaitForSeconds(delayBetweenTwoPoints);
+        var rd = new Random();
+
+        for (int i = 0; i < numberOfPoints +1; i++)
+        {
+            networkedPosition.Add(new Vector3(Random.Range(0f, 10f), Random.Range(0f, 10f), Random.Range(0f, 10f)));
+            yield return awaiter;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (positions.Count != networkedPosition.Count)
+        {
+            positions = networkedPosition.ToList();
+        }
+    }
+}
---
+++ Assets/HostGame.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MLAPI;
+using MLAPI.SceneManagement;
+using UnityEngine;
+
+public class HostGame : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            NetworkingManager.Singleton.OnClientConnectedCallback += ClientConnected;
+            NetworkingManager.Singleton.OnClientDisconnectCallback += ClientDisconnected;
+            NetworkingManager.Singleton.OnServerStarted += () => {
+                Debug.Log("Server started");
+                CreateServerEnvironment();
+            };
+            NetworkingManager.Singleton.StartServer();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            NetworkingManager.Singleton.OnClientConnectedCallback += ClientConnected;
+            NetworkingManager.Singleton.OnClientDisconnectCallback += ClientDisconnected;
+            NetworkingManager.Singleton.StartClient();
+        }
+    }
+    
+    private void CreateServerEnvironment() {
+        var progress = NetworkSceneManager.SwitchScene("InteractionScene");
+        progress.OnClientLoadedScene += id => {
+            Debug.Log("client " + id + "has changed scene");
+        };
+    }
+
+    private void ClientConnected(ulong clientId) {
+        Debug.Log($"I'm connected {clientId}");
+    }
+
+    private void ClientDisconnected(ulong clientId) {
+        Debug.Log($"I'm disconnected {clientId}");
+    }
+}
